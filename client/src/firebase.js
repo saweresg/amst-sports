22bd8045b01c11d67ee6f9880bb308d58ff03302
saweresg <65528495+saweresg@users.noreply.github.com>
2023-12-01 05:57:02 -0500

    Update firebase.js
    removed urls

--- client/src/firebase.js
+++ client/src/firebase.js
@@ -5,13 +5,6 @@ import "firebase/compat/firestore";
 console.log(process.env.REACT_APP_FIREBASE_API_KEY);
 
 const app = firebase.initializeApp({
-  // apiKey: "AIzaSyDjMJ0rpB2DkSmpgJHTMdNfJ3CS6Wre6Hs",
-  // authDomain: "amst-dev-64481.firebaseapp.com",
-  // projectId: "amst-dev-64481",
-  // storageBucket: "amst-dev-64481.appspot.com",
-  // messagingSenderId: "905547247836",
-  // appId: "1:905547247836:web:64d366ece5cf383e04c419",
-  // apiKey: "AIzaSyDjMJ0rpB2DkSmpgJHTMdNfJ3CS6Wre6Hs",
   apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
   authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
   projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
